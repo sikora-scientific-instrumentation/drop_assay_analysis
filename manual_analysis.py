6c2fdb708f460738adc8a718a759ed62fef463ea
Seb Sikora <sebsikora@nidavellir.home>
2021-03-24 15:27:43 +0000

    Continued analysis tool re-write.

--- manual_analysis.py
+++ manual_analysis.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import constants, filedialog, simpledialog
 from PIL import Image, ImageTk
 import numpy as np
-import os
 import os.path
 import csv
 
@@ -14,25 +13,29 @@ class DropAssay():
 		self.assay_path = assay_path
 		self.log_file_path = assay_path + '/log_data.csv'
 		
-		log_data = self.LoadLog(self.log_file_path)
-		for row in log_data:
-			id_number = row[1]
-			frame = {}
-			frame['events'] = []
-			frame['id'] = id_number
-			data = {}
-			data['temperature_tc'] = row[3]
-			data['temperature_prt'] = row[4]
-			data['time'] = row[0]
-			data['setpoint_temp'] = row[2]
-			frame['data'] = data
-			frame_image_path = self.assay_path + '/' + str(id_number) + '.png'
-			if os.path.exists(frame_image_path):
-				frame['image_path'] = frame_image_path
-			else:
-				print('Image file missing for frame # ' + str(id_number))
-				frame['image_path'] = 'FILE_NOT_FOUND'
-			self.frames[id_number] = frame
+		if os.path.isfile(self.log_file_path) == True:
+			self.drop_assay_loaded = True
+			log_data = self.LoadLog(self.log_file_path)
+			for row in log_data:
+				id_number = row[1]
+				frame = {}
+				frame['events'] = []
+				frame['id'] = id_number
+				data = {}
+				data['tc_temperature'] = row[3]
+				data['prt_temperature'] = row[4]
+				data['time'] = row[0]
+				data['sp_temperature'] = row[2]
+				frame['data'] = data
+				frame_image_path = self.assay_path + '/' + str(id_number) + '.png'
+				if os.path.exists(frame_image_path):
+					frame['image_path'] = frame_image_path
+				else:
+					print('Image file missing for frame # ' + str(id_number))
+					frame['image_path'] = 'FILE_NOT_FOUND'
+				self.frames[id_number] = frame
+		else:
+			self.drop_assay_loaded = False
 		
 	def LoadLog(self, log_file_path):
 		log_data = []
@@ -73,6 +76,41 @@ class DropAssayViewer():
 		self.LoadImage()
 	
 	def Done(self):
+		total_events = 0.0
+		for frame_id in self.frame_ids:
+			total_events += len(self.drop_assay.frames[frame_id]['events'])
+		
+		cumulative_events = 0.0
+		output_table = []
+		for frame_id in self.frame_ids:
+			events_this_frame = len(self.drop_assay.frames[frame_id]['events'])
+			if events_this_frame > 0:
+				cumulative_events += events_this_frame
+				fraction_frozen_this_frame = cumulative_events / total_events
+				tc_temp_this_frame = self.drop_assay.frames[frame_id]['data']['tc_temperature']
+				output_table.append([tc_temp_this_frame, fraction_frozen_this_frame])
+		
+		# If we have more than one event, total:
+		if len(output_table) > 1:
+			# Temperatures may not be monotonically increasing, sort output table by temperature to mitigate this.
+			output_table.sort()
+			
+			# Merge adjacent entries with identical temperatures (may result from previous operation).
+			merged_output_table = []
+			new_row = output_table[0]
+			for i, current_row in enumerate(output_table[1:]):
+				if current_row[0] == new_row[0]:
+					new_row[1] += current_row[1]
+				else:
+					merged_output_table.append(new_row)
+					new_row = current_row
+				# If this is the last line of the original output table we need to append new_row before we finish.
+				if i == (len(output_table[1:]) - 1):
+					merged_output_table.append(new_row)
+			output_table = merged_output_table
+		
+		print(output_table)
+		
 		self.root.destroy()
 	
 	def LoadImage(self):
@@ -84,6 +122,7 @@ class DropAssayViewer():
 		else:
 			print('No image file found for this frame number!')
 		self.AnnotateFrame()
+		self.AnnotateEvents()
 	
 	def RegisterClick(self, event):
 		print((event.x, event.y))
@@ -104,7 +143,7 @@ class DropAssayViewer():
 					break
 		if already_frozen == False:
 			self.drop_assay.frames[current_frame_id]['events'].append(freezing_event)
-			self.AnnotateFrame()
+			self.AnnotateEvents()
 		else:
 			print('Already a frozen droplet here!')
 		
@@ -126,25 +165,30 @@ class DropAssayViewer():
 		current_frame_index = self.current_frame_index
 		current_frame_id = self.frame_ids[current_frame_index]
 		frame_count = len(self.frame_ids)
-		temperature = self.drop_assay.frames[current_frame_id]['data']['temperature_tc']
+		temperature = self.drop_assay.frames[current_frame_id]['data']['tc_temperature']
+		
 		try:
 			self.canvas.delete(self.canvas_frame_counter)
-			self.canvas_frame_counter = self.canvas.create_text(5,40,fill="blue",font="Times 30 bold",text=str(current_frame_index + 1) + '/' + str(frame_count), anchor = tk.SW)
 		except:
-			self.canvas_frame_counter = self.canvas.create_text(5,40,fill="blue",font="Times 30 bold",text=str(current_frame_index + 1) + '/' + str(frame_count), anchor = tk.SW)
+			pass
+		self.canvas_frame_counter = self.canvas.create_text(5,40,fill="blue",font="Times 30 bold",text=str(current_frame_index + 1) + '/' + str(frame_count), anchor = tk.SW)
 		
 		try:
 			self.canvas.delete(self.canvas_temperature)
-			self.canvas_temperature = self.canvas.create_text(5,70,fill="blue",font="Times 20 bold",text=str(temperature) + ' °C', anchor = tk.SW)
 		except:
-			self.canvas_temperature = self.canvas.create_text(5,70,fill="blue",font="Times 20 bold",text=str(temperature) + ' °C', anchor = tk.SW)
+			pass
+		self.canvas_temperature = self.canvas.create_text(5,70,fill="blue",font="Times 20 bold",text=str(temperature) + ' °C', anchor = tk.SW)
 		
+	def AnnotateEvents(self):
+		current_frame_index = self.current_frame_index
+		current_frame_id = self.frame_ids[current_frame_index]
 		try:
 			for annotation_id in self.annotation_ids:
 				self.canvas.delete(self.annotation_id)
 			self.annotation_ids = []
 		except:
 			self.annotation_ids = []
+		self.annotation_ids = []
 		for frame_id in self.frame_ids:
 			if frame_id <= current_frame_id:
 				events_this_frame = self.drop_assay.frames[frame_id]['events']
@@ -157,104 +201,30 @@ class DropAssayViewer():
 						else:
 							annotation_id = self.canvas.create_text(x_coord, y_coord,fill="blue",font="Times 20 bold",text='O')
 						self.annotation_ids.append(annotation_id)
-
+			
 if __name__ == "__main__":
 	root = tk.Tk()
 	root.withdraw()
 	
 	assay_path = filedialog.askdirectory()
-	print(assay_path)
 	
-	drop_assay = DropAssay(assay_path)
+	failed = True
 	
-	assay_viewer = DropAssayViewer(root, drop_assay)
+	if assay_path != ():
+		print('Drop-assay ' + assay_path + ' selected')
+		drop_assay = DropAssay(assay_path)
+		if drop_assay.drop_assay_loaded == True:
+			failed = False
+			assay_viewer = DropAssayViewer(root, drop_assay)
+		else:
+			print('No log_data.csv found!')
+			failed = True
+	else:
+		print('No drop-assay selected.')
+		failed = True
+	
+	if failed == True:
+		root.destroy()
 	
 	root.mainloop()
 	
-	
-
-		#~self.coordinates.append((self.image_index, event.x, event.y))
-		#~self.canvas.create_text(event.x, event.y,fill="red",font="Times 20 bold",text='O')
-		#~self.annotate()
-		
-	#~def roundUpToInt(self, value):
-	#~# Rounds a floating point value to the nearest integer. When the value is >= x.5,
-	#~# round up.
-		#~int_value = int(value)
-		#~if value - int_value >= 0.5:
-			#~output_value = int_value + 1
-		#~else:
-			#~output_value = int_value
-		#~return output_value
-
-	#~def Done(self):
-		#~results = []
-		#~for current_frame in self.frozen_droplets:
-			#~if current_frame[1] > 0:
-				#~results.append([current_frame[1], self.log_data[current_frame[0]][1]])
-		#~print(results)
-		#~self.results = self.mergeEntries(results)
-		
-		#~#print self.results
-		#~self.root.destroy() 
-	
-	#~def mergeEntries(self, input_list):
-		#~output_list = []
-		#~subtotal_count = 0
-		#~for i, current_entry in enumerate(input_list):
-			#~initial_count = current_entry[0]
-			#~if current_entry[1] not in [j[3] for j in output_list]:
-				#~additional_counts = 0
-				#~for current_comparison in input_list[(i + 1):]:
-					#~if current_comparison[1] == current_entry[1]:
-						#~additional_counts += current_comparison[0]
-				#~subtotal_count += (initial_count + additional_counts)
-				#~output_list.append([initial_count + additional_counts, subtotal_count, float(subtotal_count)/float(self.number_droplets), input_list[i][1]])
-		#~return output_list
-	
-	#~def rgb2gray(self, rgb):
-		#~# https://stackoverflow.com/questions/12201577/how-can-i-convert-an-rgb-image-into-grayscale-in-python
-		#~#
-		#~# Allegedly, this is the same as the equivalent function in Matlab.
-		#~r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
-		#~gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
-		#~return gray
-		
-	#~def getImages(self, directory):
-		#~images = [file for file in os.listdir(directory) if file.endswith('.png')]
-		#~try:
-			#~images.sort(key=lambda f: int(filter(str.isdigit, f)))
-		#~except:
-			#~images.sort(key=lambda f: int(filter(unicode.isdigit, f)))
-		#~return images
-	
-	#~def loadLogFile(self):
-		#~log_data = []
-		#~with open(self.log_file, 'r') as csvfile:
-			#~reader = csv.reader(csvfile, delimiter = ',')
-			#~for i, row in enumerate(reader):
-				#~if not i == 0:
-					#~log_data.append([int(row[1]), float(row[3])])
-		#~return log_data
-	
-	#~def prevImage(self, event):
-		#~if self.image_index > 0:
-			#~self.image_index -= 1
-			#~self.loadImage()
-			
-	#~def nextImage(self, event):
-		#~if self.image_index < len(self.images) - 1:
-			#~self.image_index += 1
-			#~self.loadImage()
-	
-	#~def addDroplet(self, event):
-		#~self.frozen_droplets[self.image_index][1] += 1
-		#~self.frozen_count += 1
-		#~self.annotate()
-		
-	#~def removeDroplet(self, event):
-		#~if self.frozen_droplets[self.image_index][1] > 0:
-			#~self.frozen_droplets[self.image_index][1] -= 1
-			#~self.frozen_count -= 1
-		#~self.annotate()
-		
